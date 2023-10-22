1ee0ce555cb2480e23513bc59e5bd98e00fa3a4d
James <jameshowie17@outlook.com>
2023-10-22 14:11:11 +0100

    added buttons to the marketing/footer component for privacy and terms and condition policy; are not links. added Logo to marketing/logo component

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -4,14 +4,17 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["400", "600"]
-})
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 
 export const Logo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" height="40" width="40" alt="Logo" />
+      <p className={cn("font-semibold", font.className)}>
+        Jotion
+      </p>
+    </div>
+  );
+};
