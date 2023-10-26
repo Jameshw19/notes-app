d3fc0bde3623a9f154c78fb0fbaca30ff0d55b24
James <jameshowie17@outlook.com>
2023-10-26 14:24:05 +0100

    in navigation.tsx added an item component that takes the handleCreate function to allow the user to create a page there aswell

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
@@ -138,7 +139,8 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4 ">
-              <DocumentList />
+          <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
         </div>
         <div
           onMouseDown={handleMouseDown}
