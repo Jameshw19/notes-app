2846777d65849003650a77f0639f4e3e89752410
James <jameshowie17@outlook.com>
2023-10-27 15:51:29 +0100

    navbar component initial UI and dynamic loading and data query

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,17 +1,45 @@
-"use client"
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { useParams } from "next/navigation";
 
 interface NavbarProps {
-    isCollapsed: boolean;
-    onResetWidth: () => void
+  isCollapsed: boolean;
+  onResetWidth: () => void;
 }
 
-export const Navbar = ({
-    isCollapsed,
-    onResetWidth
-}: NavbarProps) => {
-    return (
-        <div>
-            NavBar
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#f1f1f1] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+            <Title initialData={document} />
         </div>
-    )
-}
+      </nav>
+    </>
+  );
+};
