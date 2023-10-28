39e94f0405dbf045f5f627da6abc92f4beef5d4f
James <jameshowie17@outlook.com>
2023-10-28 14:08:02 +0100

    /document/page.tsx initial setup

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -12,7 +12,25 @@ interface DocumentIdPageProps {
 }
 
 const DoumentIdPage = ({ params }: DocumentIdPageProps) => {
-  return <div>doucment id</div>;
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DoumentIdPage;
