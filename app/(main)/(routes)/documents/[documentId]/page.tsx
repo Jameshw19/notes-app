555a9bbcc18b023a1990e18f646b59f64a091974
James <jameshowie17@outlook.com>
2023-10-28 14:04:33 +0100

    fixed dark mode navbar color to black from white. added /documentId/page.tsx interface props

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,17 @@
-const DoumentIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DoumentIdPage = ({ params }: DocumentIdPageProps) => {
   return <div>doucment id</div>;
 };
 
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#f1f1f1] px-3 py-2 w-full flex items-center justify-between">
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
@@ -38,7 +38,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   return (
     <>
-      <nav className="bg-background dark:bg-[#f1f1f1] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
           <MenuIcon
             role="button"
