daa724c4b46712a862c26d33b8830d0638a089bb
James <jameshowie17@outlook.com>
2023-10-30 14:43:52 +0000

    added edgestore to the blocknote editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -7,6 +7,8 @@ import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { useTheme } from "next-themes";
 
+import { useEdgeStore } from "@/lib/edgestore";
+
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
@@ -14,6 +16,14 @@ interface EditorProps {
 }
 export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+    return response.url;
+  };
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
@@ -23,6 +33,7 @@ export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
+    uploadFile: handleUpload,
   });
 
   return (
